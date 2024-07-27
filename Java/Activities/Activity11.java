c77865bd562f2414d3c394646a86b86257cce6fc
raushankumarFST <raushan.kumar5@ibm.com>
2024-07-28 02:29:47 +0530

    activity submit

---
+++ Java/Activities/Activity11.java
@@ -0,0 +1,27 @@
+package activities;
+
+import java.util.HashMap;
+
+public class Activity11 {
+	public static void main(String[] args) {
+		HashMap<Integer, String> colour = new HashMap<Integer, String>();
+		colour.put(1, "Red");
+		colour.put(2, "Blue");
+		colour.put(3, "Green");
+		colour.put(4, "Black");
+		colour.put(5, "Pink");
+
+		System.out.println("Current set: " + colour);
+
+		colour.remove(4, "Black");
+		System.out.println("Updated set: " + colour);
+
+		if (colour.containsValue("Purple")) {
+			System.out.println("Purple colour is present in the set");
+		} else {
+			System.out.println("Purple colour is not present in the set");
+		}
+
+	}
+
+}
