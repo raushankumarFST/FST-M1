563aba9371695cfcdce207733d01e78395c11fc8
raushankumarFST <raushan.kumar5@ibm.com>
2024-09-25 18:13:51 +0530

    Activity upload

---
+++ Appium/Activities/Activity2.java
@@ -0,0 +1,48 @@
+package activities;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+ 
+public class Activity2 {
+    AndroidDriver driver;
+ 
+    @BeforeClass
+    public void setUp() throws MalformedURLException, URISyntaxException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.vivo.calculator");
+        options.setAppActivity(".Calculator");
+        options.noReset();
+ 
+        URL serverURL = new URI("http://localhost:4723").toURL();
+        driver = new AndroidDriver(serverURL, options);
+    }
+ 
+    @Test
+    public void multiplyTest() {
+        driver.findElement(AppiumBy.id("com.vivo.calculator:id/digit_8")).click();
+        driver.findElement(AppiumBy.accessibilityId("Multiply")).click();
+        driver.findElement(AppiumBy.id("com.vivo.calculator:id/digit_5")).click();
+ 
+        String result = driver.findElement(AppiumBy.id("com.vivo.calculator:id/result")).getText();
+ 
+        Assert.assertEquals(result, "40");
+    }
+ 
+
+    @AfterClass
+    public void tearDown() {
+
+        driver.quit();
+    }
+}
